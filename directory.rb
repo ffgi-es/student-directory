7750c12620b7e0cd90cbc19a8c508e1688395e0f
Alastair Gilles <agilles91@gmail.com>
2019-10-18 13:33:12 +0200

    refactored to use an array for student list

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
-# first print the list of students
+# put the students in a list
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates",
+]
+
+# print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each { |student| puts student }
+
 # then print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.size} great students"
